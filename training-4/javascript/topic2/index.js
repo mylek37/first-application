68eb791e38e9c66a464218088352e7223a16d034
Le My <mylek37@gmail.com>
2019-11-10 21:43:54 +0700

    index.js

---
+++ training-4/javascript/topic2/index.js
@@ -0,0 +1,21 @@
+// alert('xin chao')
+// document.getElementById('myImage').src='pic_bulbon.gif'
+// var showMessenger = () => {
+        // alert('An Nhien')};
+// showMessenger();
+
+var getAgeMortherAndAgeChild = (motherAge, childAge) => { 
+        var totalAge = motherAge + childAge; alert(totalAge) 
+                }
+        var mumAge = 26;
+        var childAge = 2;
+        getAgeMortherAndAgeChild(mumAge, childAge)
+        // geMortherAndAgeChild(mumAge, childAge);
+// var getAgeMotherAndChildren = (motherAge, childAge) => {
+//                 var totalAge = motherAge + childAge;
+//                 alert(totalAge);
+//             }
+//             /// Thuc thi ham tren
+//             var mumAge = 26;
+//             var childAge = 2;
+//             getAgeMotherAndChildren(mumAge, childAge);
