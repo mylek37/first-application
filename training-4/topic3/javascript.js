d01979e08751d51b9ec5c235e2c7eb86a7e3d0ae
Le My <mylek37@gmail.com>
2019-11-16 14:34:46 +0700

    saturday

--- training-4/topic3/javascript.js
+++ training-4/topic3/javascript.js
@@ -7,3 +7,11 @@ var changeStyleHtml = () => {
         var demoFontsize = '35px';
         demoElement.style.fontSize = demoFontsize;
 }
+var hideHtmlElement = () => {
+        var demoDisplay = 'none';
+        demoElement.style.display = demoDisplay;
+}
+var showHtmlElement = () => {
+        var demoStyle = 'block';
+        demoElement.style.display = demoStyle;
+}
