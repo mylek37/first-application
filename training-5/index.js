23d60c3f20bf7e0cc7c459cb864d83b99a31644a
vo xuan huy <mylek37@gmail.com>
2020-02-12 22:26:48 +0700

    training 5

---
+++ training-5/index.js
@@ -0,0 +1,14 @@
+var x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+console.log("VO Xuan Huy", z);
+console.log(z);
+// var totalInput = document.getElementById('total');
+// totalInput.value = x;
+var total = document.getElementById('sum');
+total.innerHTML = z;
+console.log('total:', total);
+
+
+
